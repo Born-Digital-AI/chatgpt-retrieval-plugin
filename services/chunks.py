987a395f9de590f4cd69865c1c8c7a9de12f91a5
David Dudas <daviddudas99@gmail.com>
2023-05-04 16:18:00 +0200

    add tiktoker cache

--- services/chunks.py
+++ services/chunks.py
@@ -6,6 +6,15 @@ import tiktoken
 
 from services.openai import get_embeddings
 
+
+import os
+
+tiktoken_cache_dir = "/code/services/tiktoken_cache/"
+os.environ["TIKTOKEN_CACHE_DIR"] = tiktoken_cache_dir
+
+# validate
+assert os.path.exists(os.path.join(tiktoken_cache_dir,"9b5ad71b2ce5302211f9c61530b329a4922fc6a4"))
+
 # Global variables
 tokenizer = tiktoken.get_encoding(
     "cl100k_base"
